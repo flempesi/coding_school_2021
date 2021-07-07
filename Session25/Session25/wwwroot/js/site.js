459e995b2ea8cbb2eb1f337293aa85cfe8644be6
Foteini Lempesi <flebesi@hotmail.gr>
2021-07-07 17:27:08 +0300

    changes

--- Session25/Session25/wwwroot/js/site.js
+++ Session25/Session25/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function CheckboxFunction() {
+    var checkBox = document.getElementById("checkActive");
+     
+    if (checkBox.checked == true) {
+        Model.SelectedListItem.Active=true;
+    } else {
+        Model.SelectedListItem.Active=false;
+    }
+ 
+}
